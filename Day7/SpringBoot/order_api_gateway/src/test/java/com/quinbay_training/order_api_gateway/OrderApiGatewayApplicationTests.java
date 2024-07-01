04fedf4a42ecd97c5bbd1666bf9ad678864c7f27
SRIMAYURIA16 <125955907+SRIMAYURIA16@users.noreply.github.com>
2024-07-01 13:15:29 +0530

    Add files via upload

---
+++ Day7/SpringBoot/order_api_gateway/src/test/java/com/quinbay_training/order_api_gateway/OrderApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinbay_training.order_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
