aba8f46504288f8d1a15166fed56c3162e03fc03
SRIMAYURIA16 <125955907+SRIMAYURIA16@users.noreply.github.com>
2024-07-01 13:20:06 +0530

    Add files via upload

---
+++ Day7/SpringBoot/reporting_api_gateway/src/main/java/com/quinbay_training/reporting_api_gateway/ReportingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.quinbay_training.reporting_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ReportingApiGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingApiGatewayApplication.class, args);
+	}
+}
---
+++ Day7/SpringBoot/reporting_api_gateway/src/test/java/com/quinbay_training/reporting_api_gateway/ReportingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinbay_training.reporting_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
