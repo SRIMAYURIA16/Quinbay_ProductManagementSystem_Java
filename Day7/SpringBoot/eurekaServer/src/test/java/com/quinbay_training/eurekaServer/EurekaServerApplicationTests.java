29c71b55e924938643932fd53b423efc07bc8cec
SRIMAYURIA16 <125955907+SRIMAYURIA16@users.noreply.github.com>
2024-07-01 13:21:44 +0530

    Add files via upload

---
+++ Day7/SpringBoot/eurekaServer/src/test/java/com/quinbay_training/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quinbay_training.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
